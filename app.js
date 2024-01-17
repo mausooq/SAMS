d0a70688e17d43877fc15b42dd7f41e9017a0abf
mausooq <mauchoomausooq051@gmail.com>
2024-01-17 23:23:27 +0530

    new

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const express = require("express");
-const mysql= require("mysql");
-
+const mysql= require("mysql2");
+const dotenv = require("dotenv");
 const app=express();
+dotenv.config({
+    path:'./.env'
+});
+const db = mysql.createConnection({
+    host:process.env.database_host,
+    user: process.env.database_user,
+    password:process.env.database_password,
+    database:process.env.database_database
+});
 
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL database:', err);
+    } else {
+        console.log('Connected to MySQL database');
+    }
+});
 app.get("/",(req,res)=>{
     res.send("<h1>helloe world<h1>")
 });
 
-app.listen(8080',(req,res                                                                                                                                           ) => {
-    console.log("started");
+app.listen(5000,()=>{
+    console.log("servr start on 8080")
 })
