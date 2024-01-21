f6dbced3d4ed2ae11d6611b6dc6e18068b3c7207
mausooq <mauchoomausooq051@gmail.com>
2024-01-21 20:33:25 +0530

    Update signup route to check for existing emails and hash passwords. (generated by blackbox.ai )

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const db = mysql.createConnection({
    host: process.env.database_host,
    user: process.env.database_user,
    password: process.env.database_password,
-   database: process.env.database_database
+   database: process.env.database
 });
 // Event handler for database connection error
 db.connect((err) => {
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,40 +1,49 @@
+// Import required modules
 const mysql = require("mysql2");
 const jwt = require('jsonwebtoken')
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
+const { reset } = require("nodemon");
 
 const db = mysql.createConnection({
     host: process.env.database_host,
     user: process.env.database_user,
     password: process.env.database_password,
-    database: process.env.database_database
+    database: process.env.database
  });
-exports.signup = (req,res) => {
+ // Handle signup request
+exports.signup = async (req,res) => {
     // console.log(req.body);
+
     // const usn = req.body.usn;
     // const name = req.body.name;
     // const phone = req.body.phone;
     // const email = req.body.email;
     // const password = req.body.password;
     // const cpassword = req.body.cpassword;
-    const {usn,name,phone,email,password,cpassword} = req.body;
-    
-    db.query('SELECT email FROM sams WHERE email = ?',[email],async (err,res) => {
+
+  // Destructure the request body
+  const {usn,name,phone,email,password,cpassword} = req.body;
+
+      // Check if the email already exists in the database
+    db.query('SELECT email FROM sams.student WHERE email = ?',[email],async (err,result) => {
         if(err){
             console.log(err);
         }
-        if(req.length > 0){
+        if(result.length > 0){
+                  // If the email already exists, render the signup page with an error message
             return res.render('signup',{
                 message:"Email already exists"
-            })
+            });
         }
+              // If the passwords don't match, render the signup page with an error message
         else if(password !== cpassword){
             return res.render('signup',{
                 message:'Password does not match'
-            })
+            });
         }
+              // If everything is fine, hash the password and insert the new user into the database
         let hasedPassword = await bcrypt.hash(password, 8);
         console.log(hasedPassword);
-    });
-   
+    }); 
 };
 
