08e7872d256543615cdd1671f76d13af57cc0758
mausooq <mauchoomausooq051@gmail.com>
2024-03-09 00:09:49 +0530

    marks

--- controllers/addSubject.js
+++ controllers/addSubject.js
@@ -11,33 +11,64 @@ const db = mysql.createConnection({
  exports.uploadSubject = async(req,res)=>{
    const id =req.user.id;
    const{semester,subjectName,maxMarks,subjectMarks,subjectGrade,} = req.body;
-   db.query('SELECT * FROM SEMESTER WHERE ID = ? AND semesterName = ?',[id,semester],async (err,result)=>{
+   db.query('SELECT * FROM SEMESTER WHERE ID = ? AND semesterName = ?',[id,semester],(err,result)=>{
       if(err){
          console.log(err);
       }
       
       if(result.length > 0){
-      const semId = result[0].ID;
-      await db.query('INSERT INTO SUBJECTS VALUES SET ?',{
+      const semId = result[0].semId;
+      const subId =  Math.floor(Math.random() * 90000) + 10000;
+      db.query('INSERT INTO MARKS SET ?',{
          id:id,
          semId:semId,
-         subId : Math.floor(Math.random() * 90000) + 10000,
+         subId :subId,
          subName : subjectName,
          maxMark : maxMarks,
          subMark : subjectMarks,
-         subjectGrade :  subjectGrade
+         grade :  subjectGrade
       },
       (error)=>{
          if(error){
             console.log(err);
          }
-         else{
             res.render('marks',{
                message:"marks added  successfully"
             })
-         }
-
       })
+      }
+      else{
+         const newsemId = Math.floor(Math.random() * 9000) + 1000;
+         db.query('INSERT INTO SEMESTER SET ?',{
+            id : id,
+            semId:newsemId,
+            semesterName: semester
+         },
+         (error)=>{
+            if(error){
+               console.log(error); 
+            }
+            const subId =  Math.floor(Math.random() * 90000) + 10000;
+             db.query('INSERT INTO MARKS SET ?',{
+               id:id,
+               semId:newsemId,
+               subId : subId,
+               subName : subjectName,
+               maxMark : maxMarks,
+               subMark : subjectMarks,
+               grade :  subjectGrade
+            },
+            (error)=>{
+               if(error){
+                  console.log(err);
+               }
+                  res.render('marks',{
+                  msg:"marks added  successfully"
+                  });
+               
+            })
+         }
+         )
       }
    })
  }
