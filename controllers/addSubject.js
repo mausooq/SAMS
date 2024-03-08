a85dd621e6d1637adeac8b5a3f6c97765a807d9a
mausooq <mauchoomausooq051@gmail.com>
2024-03-08 23:00:08 +0530

    marks

--- controllers/addSubject.js
+++ controllers/addSubject.js
@@ -9,7 +9,36 @@ const db = mysql.createConnection({
  });
 
  exports.uploadSubject = async(req,res)=>{
+   const id =req.user.id;
    const{semester,subjectName,maxMarks,subjectMarks,subjectGrade,} = req.body;
-   db.query('INSERT INTO SEMESTER SET ? ; ')
+   db.query('SELECT * FROM SEMESTER WHERE ID = ? AND semesterName = ?',[id,semester],async (err,result)=>{
+      if(err){
+         console.log(err);
+      }
+      
+      if(result.length > 0){
+      const semId = result[0].ID;
+      await db.query('INSERT INTO SUBJECTS VALUES SET ?',{
+         id:id,
+         semId:semId,
+         subId : Math.floor(Math.random() * 90000) + 10000,
+         subName : subjectName,
+         maxMark : maxMarks,
+         subMark : subjectMarks,
+         subjectGrade :  subjectGrade
+      },
+      (error)=>{
+         if(error){
+            console.log(err);
+         }
+         else{
+            res.render('marks',{
+               message:"marks added  successfully"
+            })
+         }
+
+      })
+      }
+   })
  }
   
